20a7b039cf6b6d1329ac7797ac3de57fc62917ce
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2020-11-08 14:27:25 +0200

    chore: add demo image

--- demo/index.ts
+++ demo/index.ts
@@ -3,8 +3,8 @@ import {resize } from '../index.ts';
 const imgJPG1 = await resize(Deno.readFileSync("./demo/input/img.jpg"), {width: 100, height: 100});
 Deno.writeFileSync('./demo/output/result.jpg', imgJPG1);
 
-const imgJPG2 = await resize(Deno.readFileSync("./demo/input/test.jpg"), {width: 100, height: 100});
-Deno.writeFileSync('./demo/output/test.jpg', imgJPG2);
+const imgJPG2 = await resize(Deno.readFileSync("./demo/input/big.jpg"), {width: 100, height: 100});
+Deno.writeFileSync('./demo/output/big.jpg', imgJPG2);
 
 const imgPNG = await resize(Deno.readFileSync("./demo/input/img.png"), {width: 100, height: 100});
 Deno.writeFileSync('./demo/output/result.png', imgPNG);
